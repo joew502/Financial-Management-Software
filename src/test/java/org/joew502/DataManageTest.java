3802e7f168300dd162774f31a9496c003cb4e195
Joe Wilson <joewilson502@gmail.com>
2023-02-03 14:36:28 +0000

    Save and Load Test

--- src/test/java/org/joew502/DataManageTest.java
+++ src/test/java/org/joew502/DataManageTest.java
@@ -6,29 +6,53 @@ import org.junit.Test;
 
 import java.io.File;
 
-import static org.junit.Assert.*;
-
 public class DataManageTest {
-
     @Test
-    public void load() {
+    public void save() {
+        // SETUP
+        Main.jsonData = new JSONObject();
+        Main.jsonData.put("Test1","1"); // Prepare data to be saved
+        DataManage.save("Test_File_1.json"); // Save test file
 
-        /*JSONObject jsonObjectSave = new JSONObject();
-        jsonObjectSave.put("testvalue","1");
-        new DataSave().save(jsonObjectSave, "", "testfile");
-        File checkFile = new File("testfile.json");
-        Assert.assertTrue(checkFile.exists());
+        // TEST
+        File testFile1 = new File("Test_File_1.json"); // Try to load file
+        Assert.assertTrue(testFile1.exists()); // Test file exists
 
-        /*JSONObject jsonObjectLoad = new DataLoad().load("testfile.json");
-
-        String testValue = (String)jsonObjectLoad.get("testvalue");
-
-        Assert.assertEquals("1",testValue);
-
-        if(checkFile.exists()){
-            if (!checkFile.delete()){
+        // TEARDOWN
+        if(testFile1.exists()){ // Delete file if it does exist
+            if (!testFile1.delete()){
                 System.out.println("Error Deleting test file");
             }
-        }*/
+        }
+        Main.jsonData.remove("Test1"); // Delete test data
+    }
+    @Test
+    public void saveAndLoad() {
+        // SETUP - Save
+        Main.jsonData = new JSONObject();
+        Main.jsonData.put("Test2", "2");
+        DataManage.save("Test_File_2.json");
+
+        // TEST - Save
+        File testFile1 = new File("Test_File_2.json");
+        Assert.assertTrue(testFile1.exists());
+
+        // SETUP - Load
+        Main.jsonData.remove("Test2"); // Delete test data
+        Main.jsonData = new JSONObject();
+        Assert.assertFalse(Main.jsonData.containsKey("Test2"));
+
+        // TEST - Load
+        DataManage.load("Test_File_2.json");
+        Assert.assertTrue(Main.jsonData.containsKey("Test2"));
+        Assert.assertEquals("2", Main.jsonData.get("Test2"));
+
+        // TEARDOWN
+        if(testFile1.exists()){
+            if (!testFile1.delete()){
+                System.out.println("Error Deleting test file");
+            }
+        }
+        Main.jsonData.remove("Test2");
     }
 }
