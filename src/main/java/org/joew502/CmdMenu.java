60df4edba791886162afe997a02d7ff75acde230
Joe Wilson <joewilson502@gmail.com>
2023-02-14 13:48:30 +0000

    View Income and Expenditure

--- src/main/java/org/joew502/CmdMenu.java
+++ src/main/java/org/joew502/CmdMenu.java
@@ -1,5 +1,7 @@
 package org.joew502;
 
+import org.json.simple.JSONObject;
+
 import java.util.Scanner;
 
 import static java.lang.System.exit;
@@ -26,7 +28,7 @@ public class CmdMenu {
                     case 1 -> DataManage.load("program_data.json");
                     case 2 -> DataManage.save("program_data.json");
                     case 3 -> inAndExpMenu();
-                    case 4 -> System.out.println("4");
+                    case 4 -> displayInAndExp();
                     case 5 -> exit(0);
                     default -> System.out.println("Please enter an integer value between 1 and 5");
                 }
@@ -36,19 +38,19 @@ public class CmdMenu {
             }
         }
     }
-    public static void inAndExpMenu() {
+    private static void inAndExpMenu() {
         Scanner menuInput = new Scanner(System.in);
         int menuChoice = 0;
         while (menuChoice != 3) {
             System.out.print("""
                                     
-                    -- Input Income or Expenditure --
-                                    
-                    1) Input Income
-                    2) Input Expenditure
-                    3) Return to Main Menu
-                                    
-                    Input the number of your choice:""");
+                -- Input Income or Expenditure --
+                                
+                1) Input Income
+                2) Input Expenditure
+                3) Return to Main Menu
+                                
+                Input the number of your choice:""");
             try {
                 menuChoice = menuInput.nextInt();
                 switch (menuChoice) {
@@ -61,6 +63,23 @@ public class CmdMenu {
                 System.out.println("Please enter an integer value between 1 and 3");
                 menuInput.next();
             }
+        }
+    }
+    private static void displayInAndExp() {
+        System.out.print("""
+                                                
+            -- Income and Expenditure --
+                            
+            INCOME:
+            """);
+        Object[] incomeKeys = ((JSONObject) Main.jsonData.get("Income")).keySet().toArray();
+        for (Object incomeKey:incomeKeys) {
+            System.out.println(incomeKey+": "+((JSONObject) Main.jsonData.get("Income")).get(incomeKey));
+        }
+        System.out.println("\nEXPENDITURE:");
+        Object[] expenditureKeys = ((JSONObject) Main.jsonData.get("Expenditure")).keySet().toArray();
+        for (Object expenditureKey:expenditureKeys) {
+            System.out.println(expenditureKey+": "+((JSONObject) Main.jsonData.get("Expenditure")).get(expenditureKey));
         }
     }
 }
--- src/main/java/org/joew502/Main.java
+++ src/main/java/org/joew502/Main.java
@@ -5,12 +5,6 @@ import org.json.simple.JSONObject;
 
 public class Main {
     public static void main(String[] args) {
-        //JSONObject jsonObject = new JSONObject();
-        //jsonObject.put("Name", "Joe");
-        //DataSave dataSave = new DataSave();
-        //String saveStatus = dataSave.save(jsonObject, "", "output");
-        //System.out.println(saveStatus);
-        //JSONObject loadedData = new DataLoad().load("output.json");
         jsonData = new JSONObject();
         jsonData.put("Income", new JSONObject());
         jsonData.put("Expenditure", new JSONObject());
