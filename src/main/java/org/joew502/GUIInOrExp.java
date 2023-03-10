02caeb75da4a7647b14ce99bad46278f18a15a0c
Joe Wilson <joewilson502@gmail.com>
2023-03-10 13:17:55 +0000

    Update GUIInOrExp.java

--- src/main/java/org/joew502/GUIInOrExp.java
+++ src/main/java/org/joew502/GUIInOrExp.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedHashMap;
 
 public class GUIInOrExp {
     public JPanel mainPanel;
@@ -25,19 +26,19 @@ public class GUIInOrExp {
 
     public void refreshData() {
 
-        Object[] incomeKeys = ((JSONObject) Main.jsonData.get("Income")).keySet().toArray();
+        Object[] incomeKeys = ((LinkedHashMap<String,Integer>) Main.jsonData.get("Income")).keySet().toArray();
         DefaultTableModel incomeModel = new DefaultTableModel();
         incomeModel.setColumnIdentifiers(new Object[]{"Income Type", "Amount"});
         for (Object incomeKey:incomeKeys) {
-            incomeModel.addRow(new Object[]{incomeKey, ((JSONObject) Main.jsonData.get("Income")).get(incomeKey)});
+            incomeModel.addRow(new Object[]{incomeKey, ((LinkedHashMap<String,Integer>) Main.jsonData.get("Income")).get(incomeKey)});
         }
         incomeTable.setModel(incomeModel);
 
-        Object[] expenditureKeys = ((JSONObject) Main.jsonData.get("Expenditure")).keySet().toArray();
+        Object[] expenditureKeys = ((LinkedHashMap<String,Integer>) Main.jsonData.get("Expenditure")).keySet().toArray();
         DefaultTableModel expenditureModel = new DefaultTableModel();
         expenditureModel.setColumnIdentifiers(new Object[]{"Expenditure Type", "Amount"});
         for (Object expenditureKey:expenditureKeys) {
-            expenditureModel.addRow(new Object[]{expenditureKey, ((JSONObject) Main.jsonData.get("Expenditure")).get(expenditureKey)});
+            expenditureModel.addRow(new Object[]{expenditureKey, ((LinkedHashMap<String,Integer>) Main.jsonData.get("Expenditure")).get(expenditureKey)});
         }
         expenditureTable.setModel(expenditureModel);
     }
